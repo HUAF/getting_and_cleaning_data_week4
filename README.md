f479f4ac95d7d440ff20f0bfa48ca4c18b162a28
huaf <hua.francois@gmail.com>
2016-09-25 21:06:29 +0200

    add codebook, working script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # getting_and_cleaning_data_week4
+
+## Packages needed
+reshape2 is needed.
+Please install it with the command below.
+install.packages(reshape2)
+
+## Running the script
+run\_analysis.R should be in the same directory as the "UCI HAR Dataset" folder.
+This directory should also be the working directory.
+
+You can run the script using the command : source("run\_analysis.R")
+
+The script will create a data\_submission\_file.txt in your working directory.
+The R working directory will also contain 3 dataframes :
+* full\_data (the merge of the test set and the train set)
+* mean\_std\_measurements (a subset of "full\_data" that contains only mean and standard deviation columns)
+* final\_dataset (the independent tidy data set)
--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,65 @@
 ## run_analysis.R
 
+#load reshape2 library
+library(reshape2)
 
-zip_file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# load needed data sets
+## activity labels
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+## features
+features <- read.table("UCI HAR Dataset/features.txt")
+## subjects
+test_subjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+train_subjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+## data files
+test_labels <- read.table("UCI HAR Dataset/test/y_test.txt")
+test_data <- read.table("UCI HAR Dataset/test/X_test.txt")
 
-## create a "data" folder if it doesn't exist yet
-if (!file.exists("./data")) {
-    dir.create("./data")
-}
+train_labels <- read.table("UCI HAR Dataset/train/y_train.txt")
+train_data <- read.table("UCI HAR Dataset/train/X_train.txt")
 
-## download the data zip file in the "data" folder
-download.file(zip_file_url, destfile = "./data/Dataset.zip")
+### bind data by rows and delete unused variables
+subjects <- rbind(test_subjects, train_subjects)
+rm(test_subjects, train_subjects)
 
-unzip("./data/Dataset.zip")
+labels <-rbind(test_labels, train_labels)
+rm(test_labels, train_labels)
+named_labels <- merge(labels, activity_labels, by.x = "V1", by.y = "V1")
+named_labels <- named_labels["V2"]
+colnames(named_labels) <- c("activity_name")
+
+data <- rbind(test_data, train_data)
+rm(test_data, train_data)
+
+## renanming col names before data final binding (AS ASKED)
+colnames(subjects) <- c("subject_id")
+colnames(labels) <- c("label_id")
+colnames(data) <- features[, 2]
+
+## bind the 3 data frames together
+
+### full_data is the merge of the test set and the training set
+full_data <- cbind(subjects, named_labels, data)
+rm(subjects, labels, data)
+
+
+# We will consider measurements on the mean to be the columns containing "mean" in their
+# column name, measurements on the standard deviation the columns containing std in their
+# column name.
+
+### 2
+# mean_std_measurements <- full_data[1, 2, grepl("(mean|std)", full_data)]
+mean_std_vect <- grep("(mean|std)", colnames(full_data))
+
+## mean_std_measurements is 
+mean_std_measurements <- full_data[c(1, 2, mean_std_vect)] # get grep outside the vector
+
+#melting the data
+melted_dataset <- melt(mean_std_measurements, id = c("subject_id", "activity_name"), measure.vars = colnames(mean_std_measurements[3:81]))
+
+final_dataset <- dcast(melted_dataset, subject_id + activity_name ~ variable, mean)
+
+write.table(final_dataset, file = "./data_submission_file.txt", row.names = FALSE)
+
+### removing unused variables
+rm(activity_labels, features, melted_dataset, named_labels)
